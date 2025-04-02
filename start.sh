5d87968c144f1da9a5d3c41679b2f5f0d66e83f7
Ricardo Rodriguez <ricardovelero@gmail.com>
2025-04-02 16:28:19 +0200

    Fix port to unify it.

--- start.sh
+++ start.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-echo "🚀 Iniciando contenedor 'sangre-ai-backend' en puerto 3000..."
+echo "🚀 Iniciando contenedor 'sangre-ai-backend' en puerto 8080..."
 
 # Cargar variables desde .env
 set -a
 source .env
 set +a
 
-docker run -d -p 3000:3000 \
+docker run -d -p 8080:8080 \
   -e NODE_ENV=$NODE_ENV \
   -e TOKEN_KEY=$TOKEN_KEY \
   -e REFRESH_TOKEN_KEY=$REFRESH_TOKEN_KEY \
   -e GEMINI_API_KEY=$GEMINI_API_KEY \
-  -e MONGO_URI="$MONGO_URI" \
+  -e MONGO_URI=$MONGO_URI \
   -e EMAILS_SECRET=$EMAILS_SECRET \
   -e FRONTEND_URL=$FRONTEND_URL \
   --name sangre-ai-backend \
