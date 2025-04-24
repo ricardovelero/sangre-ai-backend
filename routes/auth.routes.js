4608c9718e9d2afaffbdeb8b7d9dce6b65b7425d
Ricardo Rodriguez <ricardovelero@gmail.com>
2025-04-24 11:36:42 +0200

    Fix, refresh token doesn't need middleware

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -17,25 +17,16 @@ const verifyToken = require("../middleware/auth");
 var router = require("express").Router();
 
 router.post("/register", register);
-
 router.post("/login", login);
-
 router.post("/logout", logout);
-
 router.post("/logout-all", logoutAll);
-
-router.get("/user", verifyToken, getUser);
-
-router.post("/refresh", verifyToken, refreshToken);
-
-router.put("/user", verifyToken, updateUser);
-
-router.put("/user/password", verifyToken, updatePassword);
-
+router.post("/refresh", refreshToken);
 router.post("/forgot-password", forgotPassword);
-
 router.post("/reset-password", resetPassword);
 
+router.get("/user", verifyToken, getUser);
+router.put("/user", verifyToken, updateUser);
+router.put("/user/password", verifyToken, updatePassword);
 router.delete("/user", verifyToken, deleteUser);
 
 module.exports = router;
